fff0c8e56110202b5611cba14b716eca592e924e
Noxan <59417659+noxan-dev@users.noreply.github.com>
2022-08-09 10:05:32 -0400

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -190,9 +190,9 @@ LOGOUT_REDIRECT_URL = '/'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
