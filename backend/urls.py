27d0df0b60513896a5d4d2be0471a732bdd8874c
Noxan <59417659+noxan-dev@users.noreply.github.com>
2022-08-09 16:32:22 -0400

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/blog/home')),
     path('blog/', include('blog.urls'), name='blog'),
 
 ]
