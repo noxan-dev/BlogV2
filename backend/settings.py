981c6535135c32ac22050fafb606d2dea764f80b
Noxan <59417659+noxan-dev@users.noreply.github.com>
2022-08-09 16:04:19 -0400

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
