71c625f7bf8d61baba42ecacf1582357184b1e67
Noxan <59417659+noxan-dev@users.noreply.github.com>
2022-08-09 08:16:53 -0400

    fixed comment

--- backend/settings.py
+++ backend/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
+    'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
